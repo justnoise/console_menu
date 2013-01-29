35f67556f1a1bcdc1f77027b98a59e431b7a5dd2
Brendan Cox <justnoise@gmail.com>
2013-01-29 15:20:17 +0530

    added liscense, removed pdb import and added in call to verify_menu_items_type

--- console_menu.py
+++ console_menu.py
@@ -1,7 +1,27 @@
+# Copyright (C) 2013 by Brendan Cox
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 import collections
 import copy
 import time
-import pdb
+
 
 class ConsoleMenu(object):
     def __init__(self, menu_text, menu_prompt='', menu_items = None):
@@ -12,6 +32,7 @@ class ConsoleMenu(object):
             self.menu_prompt = 'Choice: '
         self.menu_items = {}
         if menu_items:
+            self._verify_menu_items_type(menu_items)
             for command, text in menu_items:
                 self.add_menu_item(command, text)
         # when we create a child, builtin commands gets extended with a back option        
